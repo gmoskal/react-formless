8dfb123d90d6c34a91e013ca2398440f4377b1fc
Grzegorz <grzegorz.moskal@tooploox.com>
2020-01-20 20:21:47 +0100

    initial commit

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    parser: "typescript",
+    semi: false,
+    singleQuote: false,
+    printWidth: 120,
+    tabWidth: 4,
+    trailingComma: "none",
+    arrowParens: "avoid",
+    jsxBracketSameLine: true
+}
